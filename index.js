5d496ffc39d48b8f1eae7e74cf8e0e5ef4603e57
Rodrigo Quezada <rodrigo.quezada@blinkhealth.com>
2021-06-24 18:48:20 -0400

    feat: test search

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ECRClient, ListTagsForResourceCommand } = require("@aws-sdk/client-ecr");
+const { STSClient, GetCallerIdentityCommand } = require("@aws-sdk/client-sts");
 const core = require('@actions/core');
 
 const app = async () => {
@@ -17,13 +18,23 @@ const app = async () => {
 
     const client = new ECRClient({ region });
 
-    // Set ListTagsForResourceCommandInput
-    const resourceArn = core.getInput('registry');
-    console.log({ resourceArn: resourceArn.split('').join('-') })
-    const params = {
-      /** input parameters */
-      resourceArn
+    const stsClient = new STSClient({ region });
+    const callerIdCommand = new GetCallerIdentityCommand();
+    const callerIdResponse = await stsClient.send(callerIdCommand);
 
+    // Set ListTagsForResourceCommandInput
+    const registry = core.getInput('registry');
+
+    // 038098751075.dkr.ecr.us-east-1.amazonaws.com/microservice-bedrock-camunda
+
+    const name = registry.split('/')[1];
+
+    const resourceArn = `arn:aws:ecr:${region}:${callerIdResponse.Account}:repository/${name}`;
+
+    console.log({ resourceArn })
+
+    const params = {
+      resourceArn
     };
     const command = new ListTagsForResourceCommand(params);
 
