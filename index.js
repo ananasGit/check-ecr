7a3fdf0cb534d74b7ef24cbef83ca90083fca4f3
Rodrigo Quezada <rodrigo.quezada@blinkhealth.com>
2021-06-24 17:14:19 -0400

    fix: add root lebel async function

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const { ECRClient, ListTagsForResourceCommand } = require("@aws-sdk/client-ecr");
 const core = require('@actions/core');
 
-try {
-  //   // `who-to-greet` input defined in action metadata file
-  //   const nameToGreet = core.getInput('who-to-greet');
-  //   console.log(`Hello ${nameToGreet}!`);
-  //   const time = (new Date()).toTimeString();
-  //   core.setOutput("time", time);
-  //   // Get the JSON webhook payload for the event that triggered the workflow
-  //   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //   console.log(`The event payload: ${payload}`);
+const app = async () => {
+  try {
+    //   // `who-to-greet` input defined in action metadata file
+    //   const nameToGreet = core.getInput('who-to-greet');
+    //   console.log(`Hello ${nameToGreet}!`);
+    //   const time = (new Date()).toTimeString();
+    //   core.setOutput("time", time);
+    //   // Get the JSON webhook payload for the event that triggered the workflow
+    //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //   console.log(`The event payload: ${payload}`);
 
-  // Configure the client
-  const region = core.getInput('region');
+    // Configure the client
+    const region = core.getInput('region');
 
-  const client = new ECRClient({ region });
+    const client = new ECRClient({ region });
 
-  // Set ListTagsForResourceCommandInput
-  const resourceArn = core.getInput('registry');
-  const params = {
-    /** input parameters */
-    resourceArn
+    // Set ListTagsForResourceCommandInput
+    const resourceArn = core.getInput('registry');
+    const params = {
+      /** input parameters */
+      resourceArn
 
-  };
-  const command = new ListTagsForResourceCommand(params);
+    };
+    const command = new ListTagsForResourceCommand(params);
 
-  const data = await client.send(command);
-  
-  console.log(data)
+    const data = await client.send(command);
 
-} catch (error) {
-  core.setFailed(error.message);
+    console.log(data)
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+app()
