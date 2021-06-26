daef64ce931695682205da8ab8ce3cc35cae72f7
Rodrigo Quezada <rodrigo.quezada@blinkhealth.com>
2021-06-26 00:14:34 -0400

    feat: set output

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-const { ECRClient, ListImagesCommand } = require("@aws-sdk/client-ecr");
-const { STSClient, GetCallerIdentityCommand } = require("@aws-sdk/client-sts");
-const core = require('@actions/core');
+const { ECRClient, BatchGetImageCommand } = require("@aws-sdk/client-ecr");
+const core = require("@actions/core");
 
 const app = async () => {
   try {
     // Configure the client
-    const region = core.getInput('region');
+    const region = core.getInput("region");
 
     const client = new ECRClient({ region });
 
-    const stsClient = new STSClient({ region });
-    const callerIdCommand = new GetCallerIdentityCommand();
-    const callerIdResponse = await stsClient.send(callerIdCommand);
-
     // Set ListImagesCommandInput
-    const registry = core.getInput('registry');
+    const registry = core.getInput("registry");
 
-    // 038098751075.dkr.ecr.us-east-1.amazonaws.com/microservice-bedrock-camunda
-
-    const name = registry.split('/')[1];
-
-    const resourceArn = `arn:aws:ecr:${region}:${callerIdResponse.Account}:repository/${name}`;
-
-    console.log({ resourceArn })
-
-    const tag = core.getInput('registry');
+    // Get id from registry URL
+    const repositoryName = registry.split("/")[1];
+    const imageTag = core.getInput("registry");
 
     const params = {
-      repositoryName: name,
-      filter: tag
+      repositoryName,
+      imageIds: [{ imageTag }],
     };
 
-    console.log({ params })
-
-    const command = new ListImagesCommand(params);
+    const command = new BatchGetImageCommand(params);
 
     const data = await client.send(command);
 
-    console.log(data)
-
+    // Set the output if the image has been found
+    if (data?.images?.length >= 1) {
+      core.setOutput("found", "true");
+    } else {
+      core.setOutput("found", "false");
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
-app()
+app();
