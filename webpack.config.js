37621793da418a2e76ba1c7c8d35d33ea5659fc2
Rodrigo Quezada <rodrigo.quezada@blinkhealth.com>
2021-06-28 21:25:39 -0400

    feat: add babel to support conditonal chaining

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-    },
-    target: 'node',
-    node: false,
-}
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  target: "node",
+  node: false,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
